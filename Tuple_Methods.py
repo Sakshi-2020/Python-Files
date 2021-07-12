cb457df620ab52363b5973aed8fe189389d3a795
Sakshi Singh <68339450+Sakshi-2020@users.noreply.github.com>
2021-07-12 21:23:56 +0530

    Add files via upload

---
+++ Tuple_Methods.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 21:10:55 2021
+
+@author: ssing
+"""
+
+#count
+numbers = (1, 8, 7, 8, 7, 5, 4, 6, 8, 5, 9)
+cnt1 = numbers.count(5)
+cnt2 = numbers.count(8)
+cnt3 = numbers.count(7)
+print(cnt1)
+print(cnt2)
+print(cnt3)
+
+#index
+x = numbers.index(9)
+print(x)
